768990b4360c8c93bfbd39556ca3c3beaae1cbb4
Toni Okuogume <toni.okuogume@futurice.com>
2018-03-19 10:33:53 +0200

    Change diff colors to match with linum setup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -634,4 +634,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-hl-change ((t (:background "#3a81c3"))))
+ '(diff-hl-delete ((t (:background "#ee6363"))))
+ '(diff-hl-insert ((t (:background "#7ccd7c")))))
