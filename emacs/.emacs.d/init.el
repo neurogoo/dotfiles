7974fe5acf13e817a9271546f1aace201b550f91
Toni Okuogume <toni.okuogume@futurice.com>
2019-03-26 14:46:50 +0200

    Update melpa address

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,7 +3,7 @@
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives
-	       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	       '("melpa" . "http://melpa.org/packages/") t)
   (add-to-list 'package-archives
 	       '("melpa-stable" . "http://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
